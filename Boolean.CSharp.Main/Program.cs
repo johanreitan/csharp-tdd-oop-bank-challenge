2611c86afda47b3f6c381de56744152a0e77b2b0
Johan Reitan <johan.s.reitan@gmail.com>
2025-08-13 10:29:16 +0200

    red tests for first two user stories

--- Boolean.CSharp.Main/Program.cs
+++ Boolean.CSharp.Main/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Boolean.CSharp.Main.Abstract;
+using Boolean.CSharp.Main.Concrete;
+
+Customer customer1 = new Customer("Johan Reitan");
+customer1.CreateCurrentAccount();
+Account savingsAccount1 = new Savings(customer1);
+savingsAccount1.Deposit(100m);
+
+Console.WriteLine(savingsAccount1.Transactions.Count);
+
--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -1,4 +1,5 @@
 ﻿using Boolean.CSharp.Main;
+using Boolean.CSharp.Main.Concrete;
 using NUnit.Framework;
 
 namespace Boolean.CSharp.Test
@@ -15,9 +16,23 @@ namespace Boolean.CSharp.Test
         }
 
         [Test]
-        public void TestQuestion1()
+        public void CreatingACurrentAccount()
         {
+            Customer customer1 = new Customer("johan");
 
+            Current currentAccount1 = customer1.CreateCurrentAccount();
+
+            Assert.That(currentAccount1.AccountOwner.Id, Is.EqualTo(customer1.Id));
+        }
+
+        [Test]
+        public void CreatingASavingsAccount()
+        {
+            Customer customer1 = new Customer("johan");
+
+            Savings savingsAccount1 = customer1.CreateSavingsAccount();
+
+            Assert.That(savingsAccount1.AccountOwner.Id, Is.EqualTo(customer1.Id));
         }
 
     }
